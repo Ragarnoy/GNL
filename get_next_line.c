4db0206f92ff71a2b0cdacc6c803a5d1a1e075d6
Tiago LERNOULD <tiagolernould@hotmail.com>
2017-09-28 23:13:22 +0200

    v0.1

---
+++ get_next_line.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlernoul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/20 21:28:37 by tlernoul          #+#    #+#             */
+/*   Updated: 2017/09/28 23:10:24 by tlernoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		get_next_line(const int fd, char **line)
+{
+	char str[BUFF_SIZE + 1];
+	int i;
+	int end;
+
+	i = 0;
+	*line = NULL;
+	while ((end = read(fd, str, BUFF_SIZE)) > 0)
+	{
+		str[BUFF_SIZE] = '\0';
+		*line = ft_strappend(*line, str, 'f');
+		if (ft_strchr(str, '\n'))
+			return (1);
+	}
+	return (0);
+}
+
+#include <fcntl.h>
+int main(int argc, const char *argv[])
+{
+	char	*line;
+	int		fd;
+
+	fd = 0;
+	if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+		ft_putendl(line);
+	return 0;
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlernoul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/20 21:21:51 by tlernoul          #+#    #+#             */
+/*   Updated: 2017/09/28 22:54:15 by tlernoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 10
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include <string.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
