7639b3d74ef4a22ec5c4973e1ca0e73f670ca42a
Tiago LERNOULD <tiagolernould@hotmail.com>
2017-10-02 19:22:41 +0200

    Restructuration

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,44 @@
 /*   By: tlernoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 21:28:37 by tlernoul          #+#    #+#             */
-/*   Updated: 2017/10/01 19:55:33 by tlernoul         ###   ########.fr       */
+/*   Updated: 2017/10/02 19:15:30 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+char	*findnewline_cut(char *str)
+{
+	char	*ret;
+
+	ret = ft_strnew(BUFF_SIZE);
+	if (!(ft_strchr(str, '\n')))
+		return (NULL);
+	else
+	{
+		ret = ft_memmove(str, ft_strchr(str, '\n') + 1, ft_strclen(str, '\n'));
+		ft_bzero(ft_strchr(str, '\n'), ft_strlen(ft_strchr(str, '\n')));
+	}
+		//printf("=+%s+=\n", str);
+	return (ret);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	static char	str[BUFF_SIZE + 1];
-	int			i;
+	static char	*str = NULL;
+	char		*tmp;
 	int			end;
-	void		*lol;
 
-	i = 0;
-	*line = NULL;
+	if (!str && !(str = ft_strnew(BUFF_SIZE)))
+		return (0);
 	while ((end = read(fd, str, BUFF_SIZE)) > 0)
 	{
-		str[BUFF_SIZE] = '\0';
-		*line = ft_strappend(*line, str, 'f');
-		printf("- %s\n", line[0]);
-		ft_strclr(str);
-		lol = ft_strchr(line[0], '\n');
-		printf("+ %s\n", lol);
-		if (lol && ft_memcpy(str, ft_strchr(*line, '\n'), ft_strclen(*line, '\n')))
+		tmp = ft_strnew(BUFF_SIZE);
+		tmp = ft_strappend(*line, str, 'f');
+		ft_bzero(str, BUFF_SIZE);
+		if (findnewline_cut(tmp))
 		{
-			ft_putstr("lol");
-			str[BUFF_SIZE] = '\0';
+			*line = ft_strappend(*line, str, 'f');
 			return (1);
 		}
 	}
@@ -52,5 +63,6 @@ int main(int argc, const char *argv[])
 		fd = open(argv[1], O_RDONLY);
 	while (get_next_line(fd, &line) > 0)
 		ft_putendl(line);
+	free(line);
 	return 0;
 }
