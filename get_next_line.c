11db0ee466aefe8d94bcde9a5fbbfb149ef91c1b
Tiago LERNOULD <tiagolernould@hotmail.com>
2017-10-04 21:32:23 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tlernoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 21:28:37 by tlernoul          #+#    #+#             */
-/*   Updated: 2017/10/03 19:07:39 by tlernoul         ###   ########.fr       */
+/*   Updated: 2017/10/04 21:32:17 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,18 @@ void	newline(char *str, char *line)
 {
 	size_t	i;
 	char	*p;
-
+printf("--------\nDebug 1 \n>>> line=%s0\n>>>> str=%s0\n--------\n\n",line,str);
 	p = ft_strchr(str, '\n');
 	i = str + ft_strlen(str) - p;
-	ft_memcpy(line, str, i);
+	if (str[0] == '\n')
+	{
+		ft_memmove(str, str + 1, i);
+		return;
+	}
+	ft_memcpy(line + ft_strlen(line), str, i);
 	line[i] = '\0';
 	ft_memmove(str, p + 1, i);
+printf("--------\nDebug 2 %zu\n>>> line=%s0\n>>>> str=%s0\n--------\n\n",i,line,str);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -32,12 +38,13 @@ int		get_next_line(const int fd, char **line)
 
 	if (/*!str &&*/!(str = ft_strnew(BUFF_SIZE)))
 		return (0);
+	*line = ft_strnew(50); //lol je sais pas
 	while ((end = read(fd, str, BUFF_SIZE)) > 0)
 	{
-		if (!line && str)
-			*line = ft_strappend(*line, str, 'f');
-		if (!ft_strchr(str, '\n'))
+printf("--------\nDebug 0 \n>>> line=%s0\n>>>> str=%s0\n--------\n\n",*line,str);
+		if (ft_strchr(str, '\n'))
 		{
+printf("======================\nPassing into newline !\n======================\n");
 			newline(str, *line);
 			return (1);
 		}
