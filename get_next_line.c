e3f0b3ede39964a2765c5e2ac7b20386457383ed
Tiago LERNOULD <tiagolernould@hotmail.com>
2017-10-06 20:13:09 +0200

    help

--- get_next_line.c
+++ get_next_line.c
@@ -6,67 +6,58 @@
 /*   By: tlernoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 21:28:37 by tlernoul          #+#    #+#             */
-/*   Updated: 2017/10/05 23:10:47 by tlernoul         ###   ########.fr       */
+/*   Updated: 2017/10/06 20:12:30 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	newline(char *str, char *line)
+void	newline(char *str, char *line, char *save)
 {
 	size_t	i;
 	char	*p;
+
 printf("--------\nDebug 1 \n>>> line=%s %zu\n>>>> str=%s %zu\n--------\n",line,ft_strlen(line),str,ft_strlen(str));
 	p = ft_strchr(str, '\n');
 	i = str + ft_strlen(str) - p;
-	if (str[0] == '\n')
+	save = ft_strnew(i);
+	if (str[0] == '\n' && str[1] != '\n')
 	{
 		ft_memmove(str, str + 1, i);
 		return;
 	}
 	*p = '\0';
 	ft_memcpy(line + ft_strlen(line), str, i);
-	//line[ft_strlen(line) + 1] = '\0';
-	ft_memmove(str, p + 1, i);
-printf("--------\nDebug 2 \n>>> line=%s %zu\n>>>> str=%s %zu\n--------\n",line,ft_strlen(line),str,ft_strlen(str));
-}
-
-void	total_recall(char *str, char *line)
-{
-	if (str && !ft_strchr(str, '\n'))
-		line = ft_strappend(line, str, 'f');
+	ft_memmove(save, p + 1, i);
+printf("--------\nDebug 1 \n>>> line=%s %zu\n>>>> str=%s %zu\n--------\n",line,ft_strlen(line),str,ft_strlen(str));
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	*str = NULL;
+	static char	*save = NULL;
+	char		buffer[BUFF_SIZE + 1];
 	int			end;
 
-	if (!str && !(str = ft_strnew(BUFF_SIZE)))
-		return (0);
 	*line = ft_strnew(BUFF_SIZE); //lol je sais pas
-	total_recall(str, *line);
-	while ((end = read(fd, str, BUFF_SIZE)) > 0)
+	while (!ft_strchr(buffer, '\n'))
 	{
-printf("--------\nDebug 0 \n>>> line=%s %zu\n>>>> str=%s %zu\n--------\n\n",*line,ft_strlen(*line),str,ft_strlen(str));
-		if (ft_strchr(str, '\n'))
-		{
-printf("======================\nPassing into newline !\n======================\n");
-			newline(str, *line);
-printf("==================\nQuitting newline !\n==================\n");
-			return (1);
-		}
-		else
-			*line = ft_strappend(*line, str, 'f');
+printf("--------\nDebug 0 \n>>> line=%s %zu\n>>>> str=%s %zu\n--------\n\n",*line,ft_strlen(*line),buffer,ft_strlen(buffer));
+		if (save[1])
+			*line = ft_strappend(*line, save, 'f');
+		if (buffer[1])
+			*line = ft_strappend(*line, buffer, 'f');
+		(end = read(fd, buffer, BUFF_SIZE));
+		save = NULL;
 	}
-	if (!line && !end)
+		newline (buffer, *line, save);
+printf("--------\nSortie NL \n>>> line=%s %zu\n>>>> str=%s %zu\n--------\n\n",*line,ft_strlen(*line),buffer,ft_strlen(buffer));
+	return (1);
+	if (!end)
 	{
-		free(str);
-		str = NULL;
 		printf("str is free\n");
+		return (0);
 	}
-	return (0);
 }
 
 #include <fcntl.h>
@@ -79,8 +70,10 @@ int main(int argc, const char *argv[])
 	if (argc == 2)
 		fd = open(argv[1], O_RDONLY);
 	while (get_next_line(fd, &line) > 0)
+	{
 		ft_putendl(line);
-	free(line);
+		free(line);
+	}
 	line = NULL;
 	return 0;
 }
