c5f5f58bfb12db95d08e13f03e7f8e82a3c275a7
Tiago LERNOULD <tiagolernould@hotmail.com>
2017-10-31 19:11:35 +0100

    God gives me strength

--- 1get_next_line.c
+++ 1get_next_line.c
@@ -6,53 +6,27 @@
 /*   By: tlernoul <tlernoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/24 20:39:50 by tlernoul          #+#    #+#             */
-/*   Updated: 2017/10/25 18:06:08 by tlernoul         ###   ########.fr       */
+/*   Updated: 2017/10/31 18:37:35 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-t_gnl	*lstnew(const char *save, int fd)
+char	*concaten(char **buffer, t_gnl *gnl)
 {
-	t_gnl *elem;
-
-	if (!(elem = malloc(sizeof(t_list))))
-		return (NULL);
-	if (!(elem->save = malloc(sizeof(elem->save) + (ft_strlen(save) + 1))))
-		return (NULL);
-	if (!(save))
-	{
-		elem->save = NULL;
-		elem->fd = 0;
-	}
-	else
-	{
-		ft_strcpy(elem->save, save);
-		elem->fd = fd;
-	}
-	elem->next = NULL;
-	return (elem);
-}
-
-char	*concaten(char **buffer)
-{
-	char *save;
-
-	save = ft_strsub(*buffer, ft_strchr(*buffer, '\n') - *buffer + 1,
-			ft_strclenc(*buffer, '\n', '\0'));
-	if
+	gnl->save[gnl->fd] = ft_strsub(*buffer, ft_strchr(*buffer, '\n')
+			- *buffer + 1,ft_strclenc(*buffer, '\n', '\0'));
 
 }
 
 int		ft_get_next_line(const int fd, char **line)
 {
-	static char	*save = NULL;
+	static t_gnl	gnl;
 	char			*buffer;
-	int				end;
 
 	*line = NULL;
-	while ((end = read(fd, buffer, BUFF_SIZE)) > 0)
+	while ((gnl.end = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		if (*buffer)
 			*line = ft_strappend(*line, buffer, 'f');
@@ -60,10 +34,9 @@ int		ft_get_next_line(const int fd, char **line)
 		{
 			*line = ft_strappend(*line, ft_strsub(buffer, 0,
 						ft_strchr(buffer, '\n') - buffer), 'f');
-			save = lstnew(concaten(&buffer), fd);
 			ft_strdel(&buffer);
 			return (1);
 		}
 	}
-	return (end || (line && *line));
+	return (gnl.end || (line && *line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: tlernoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 21:21:51 by tlernoul          #+#    #+#             */
-/*   Updated: 2017/10/24 21:07:51 by tlernoul         ###   ########.fr       */
+/*   Updated: 2017/10/31 18:36:56 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 10
+# define FD_MAX 7168
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -21,9 +22,9 @@
 
 typedef struct		s_gnl
 {
-	char			*save;
+	char			*save[FD_MAX];
+	int				end;
 	int				fd;
-	struct s_gnl	*next;
 }					t_gnl;
 
 int					get_next_line(const int fd, char **line);
